08fd4f192de14549a626b60f67f4aef9e01ffc82
MariaMGV <mimutu.vasileva@gmail.com>
2024-02-26 16:49:08 +0200

    Initial commit

---
+++ src/main/java/introExercise/MyExerciseClass.java
@@ -0,0 +1,16 @@
+package introExercise;
+
+public class MyExerciseClass
+{
+    public static void printHello()
+    {
+        System.out.println("Hello, world! This is from the method");
+    }
+
+    public static void main(String[] args) {
+        printHello();
+
+    }
+
+}
+
